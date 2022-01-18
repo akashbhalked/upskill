0a7b5af723f257516dd8ce1db372f52cdb49f35c
akash bhalke <akash.bhalke@spicemoney.com>
2022-01-18 15:28:12 +0530

    binary search

--- DSA/logicmojo/binarySearch/binarySearch.js
+++ DSA/logicmojo/binarySearch/binarySearch.js
@@ -31,3 +31,45 @@ function binarySearch(arr, val, start = 0, end = arr.length - 1) {
 
 console.log(binarySearch([1, 2, 3, 4, 5, 6], 8)); // -1
 console.log(binarySearch([1, 2, 3, 4, 5, 6], 2)); //2
+
+
+//One array of integers is given as an input ,which is initially increasing and then decreasing or it can be only 
+//increasing or decreasing, you need to find the maximum value in the array in O(Log n) 
+//Time complexity and O(1) Space Complexity Asked in
+function findMaximumValue(array, low, high) {
+    /*Only one element is present in array1[low..high]*/
+    if (low == high)
+        return array[low];
+
+    /* If there are two elements and first is greater then  
+        the first element is maximum */
+    if ((high == low + 1) && array[low] >= array[high])
+        return array[low];
+
+    /* If there are two elements and second is greater then  
+        the second element is maximum */
+    if ((high == low + 1) && array[low] < array[high])
+        return array[high];
+
+    let mid = Math.floor((low + high) / 2); /*low + (high - low)/2;*/
+
+    /* If we reach a point where array1[mid] is greater than both of  its adjacent elements array1[mid-1] and array1[mid+1], then array1[mid]  
+        is the maximum element*/
+    if (array[mid] > array[mid + 1] && array[mid] > array[mid - 1])
+        return array[mid];
+
+    /* If array1[mid] is greater than the next 
+        element and smaller than the previous  
+        element then maximum lies on left side of mid */
+    if (array[mid] > array[mid + 1] && array[mid] < array[mid - 1])
+        return findMaximumValue(array, low, mid - 1);
+
+    // when array1[mid] is greater than array1[mid-1] 
+    else
+        return findMaximumValue(array, mid + 1, high);
+}
+
+
+
+let arr = [1, 3, 50, 10, 9, 7, 6]
+console.log(findMaximumValue(arr, 0, arr.length - 1)) //50
